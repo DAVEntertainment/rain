594fffacc52aa312cadfdb45b8a3f1d8ebe5d560
davied <wuwei_543333827@126.com>
2022-10-29 11:56:35 +0800

    fix lint problems

--- ci/pyutils/shell_utils.py
+++ ci/pyutils/shell_utils.py
@@ -1,6 +1,18 @@
+"""
+shell utils
+"""
 from subprocess import Popen
 
 def run_cmd(cmd):
+    """
+    run cmd with log
+
+    signature:
+        run_cmd(cmd)
+
+    params:
+        cmd         command to execute
+    """
     print(cmd)
     with Popen(cmd) as proc:
         proc.communicate()
