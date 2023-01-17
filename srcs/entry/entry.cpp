23fca3f7838807a0c045f45a9cc955f4be37cec9
davied <wuwei_543333827@126.com>
2023-01-17 21:34:57 +0800

    fix a little bit for cmd line

--- srcs/entry/entry.cpp
+++ srcs/entry/entry.cpp
@@ -14,41 +14,41 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <memory>
 #include "rain/rain.h"
 
 int main(int nargs, char* args[]) {
     if (1 == nargs) {
         rain::StringLogoFactory factory;
-        auto logo = factory.Create(factory.kRain);
+        std::shared_ptr<rain::IStringLogo> logo(factory.Create(factory.kRain));
         std::cout << logo->ToString() << std::endl;
-        delete logo;
-    } else if (2 == nargs) {
-        std::string file(args[1]);
-        std::cout << "using " << file << std::endl;
-        std::ifstream istm(file, std::ios::in | std::ios::binary);
-        if (!istm.is_open()) {
-            std::cout << "failed to open file" << std::endl;
-        } else {
-            std::stringstream ss;
-            ss << istm.rdbuf();
-            istm.close();
-            std::string content = ss.str();
-            std::cout << content << std::endl;
-            rain::InterpreterFactory factory;
-            auto interpreter = factory.Create(factory.kRain);
-            interpreter->Parse(content);
-            delete interpreter;
+        return 0;
+    }
+
+    std::string versionTag("--version");
+    for (int i = 0; i < nargs; ++i) {
+        if (0 == versionTag.compare(args[i])) {
+            rain::VersionFactory factory;
+            std::shared_ptr<rain::IVersion> v(factory.Create(factory.kCurrent));
+            std::cout << v->GetFullVersion() << std::endl;
+            return 0;
         }
+    }
+
+    std::string file(args[1]);
+    std::cout << "using " << file << std::endl;
+    std::ifstream istm(file, std::ios::in | std::ios::binary);
+    if (!istm.is_open()) {
+        std::cout << "failed to open file" << std::endl;
     } else {
-        std::string versionTag("--version");
-        for (int i = 0; i < nargs; ++i) {
-            if (0 == versionTag.compare(args[i])) {
-                rain::VersionFactory factory;
-                auto v = factory.Create(factory.kCurrent);
-                std::cout << v->GetFullVersion() << std::endl;
-                delete v;
-            }
-        }
+        std::stringstream ss;
+        ss << istm.rdbuf();
+        istm.close();
+        std::string content = ss.str();
+        std::cout << content << std::endl;
+        rain::InterpreterFactory factory;
+        std::shared_ptr<rain::IInterpreter> interpreter(factory.Create(factory.kRain));
+        interpreter->Parse(content);
     }
     return 0;
 }
